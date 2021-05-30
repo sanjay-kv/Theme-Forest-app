abb31502a9cf225de863d74b53c7d4b61c9795b3
Sanjay KV <sanjay.kanakkot@gmail.com>
2021-05-31 02:58:47 +0530

    Project Completed.

---
+++ app/src/main/java/com/example/themeforestwallpaper/FullimageActivity.java
@@ -0,0 +1,53 @@
+package com.example.themeforestwallpaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+
+public class FullimageActivity extends AppCompatActivity {
+
+    private ImageView fullimage;
+    private Button apply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullimage);
+
+        fullimage = findViewById(R.id.fullimage);
+        apply= findViewById(R.id.apply);
+
+        Glide.with(this).load(getIntent().getStringExtra("image")).into(fullimage);
+
+        apply.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setBackground();
+
+            }
+        });
+
+    }
+
+    private void setBackground() {
+        Bitmap bitmap =((BitmapDrawable)fullimage.getDrawable()).getBitmap();
+        WallpaperManager manager= WallpaperManager.getInstance(getApplicationContext());
+        try {
+            manager.setBitmap(bitmap);
+        } catch (IOException e) {
+            Toast.makeText(this, "Error:"+e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/example/themeforestwallpaper/MainActivity.java
+++ app/src/main/java/com/example/themeforestwallpaper/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.themeforestwallpaper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private DatabaseReference reference;
+    private ArrayList<String> list;
+    private WallpaperAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("images");
+        recyclerView = findViewById(R.id.recyclerView);
+        progressBar = findViewById(R.id.progressBar);
+
+        getData();
     }
+
+    private void getData() {
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                progressBar.setVisibility((View.GONE));
+                list= new ArrayList<>();
+
+                for (DataSnapshot shot : snapshot.getChildren()) {
+                    String data = shot.getValue().toString();
+                    list.add(data);
+                }
+
+                recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
+                adapter=new WallpaperAdapter(list, MainActivity.this);
+                recyclerView.setAdapter((adapter));
+                progressBar.setVisibility(View.GONE);
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                progressBar.setVisibility((View.GONE));
+                Toast.makeText(MainActivity.this, "Error :"+error.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+
 }
